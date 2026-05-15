06645e68ac6482b31f1c427ed1b61134de36fb21
Farid Gasimov <farid.gasimov95@gmail.com>
2026-05-15 10:39:06 +0400

    feat: finished history page containing all processed files

--- frontend/src/pages/History.tsx
+++ frontend/src/pages/History.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { HistoryFile } from "../types/history";
-import { getHistory } from "../api/historyApi";
+import { downloadFile, getHistory } from "../api/historyApi";
+import {
+  tableCellClass,
+  tableHeaderClass,
+  tableRowClass,
+} from "../constants/styles";
 
 export default function HistoryPage() {
   const [history, setHistory] = useState<HistoryFile[]>([]);
@@ -20,15 +25,44 @@ export default function HistoryPage() {
     fetchHistory();
   }, []);
 
+  async function handleDownload(id: number) {
+    const { blob, filename } = await downloadFile(id, user!.token);
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    a.click();
+    URL.revokeObjectURL(url);
+  }
+
   return (
     <div className="flex h-full bg-background items-start justify-center pt-16">
-      <div className=" bg-surface border border-border p-8 rounded-2xl shadow-sm">
-        <h1 className="text-xl font-semibold">History</h1>
-        <ul>
-          {history.map((file) => {
-            return <li key={file.id}>{file.filename}</li>;
-          })}
-        </ul>
+      <div className=" bg-surface border border-border p-8 rounded-2xl shadow-sm w-[45rem]">
+        <h1 className="text-xl font-semibold mb-4 ">History</h1>
+        <table className="w-full">
+          <thead>
+            <tr className={tableRowClass}>
+              <th className={tableHeaderClass}>Filename</th>
+              <th className={tableHeaderClass}>Date</th>
+              <th className={tableHeaderClass}></th>
+            </tr>
+          </thead>
+          <tbody>
+            {history.map((file) => (
+              <tr key={file.id} className={tableRowClass}>
+                <td className={tableCellClass}>{file.filename}</td>
+                <td className={tableCellClass}>
+                  {new Date(file.savedAt).toLocaleString()}
+                </td>
+                <td className={tableCellClass}>
+                  <button onClick={() => handleDownload(file.id)}>
+                    Download
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
